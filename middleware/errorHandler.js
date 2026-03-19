add7057dc0875853bf6187154be34904454a7dbb
dedlinux777 <dedlinux@gmail.com>
2026-03-19 13:16:26 +0530

    removed redundant's from rooot folder

--- middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error("ERROR:", err); // 👈 log full error on server
-
-  res.status(err.statusCode || 500).json({
-    message: err.message || "Something went wrong"
-  });
-};
-
-module.exports = errorHandler;
